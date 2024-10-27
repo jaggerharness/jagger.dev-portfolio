8c567d8bd3fdfd0c0ceb253d96a51dc84f7c206d
jaggerharness <jagger.dev@gmail.com>
2024-10-27 14:38:42 -0500

    feat: sitemap generation - security headers

--- next.config.mjs
+++ next.config.mjs
@@ -18,20 +18,16 @@ const nextConfig = {
             source: '/(.*)',
             headers: [
               {
-                key: 'X-Frame-Options',
-                value: 'SAMEORIGIN',
-              },
-              {
                 key: 'X-Content-Type-Options',
-                value: 'nosniff',
+                value: 'nosniff'
               },
               {
-                key: 'Content-Security-Policy',
-                value: cspHeader.replace(/\n/g, ''),
+                key: 'X-Frame-Options',
+                value: 'DENY'
               },
               {
-                key: 'Referrer-Policy',
-                value: 'origin-when-cross-origin'
+                key: 'X-XSS-Protection',
+                value: '1; mode=block'
               },
               {
                 key: 'Permissions-Policy',
--- src/app/components/timeline.tsx
+++ src/app/components/timeline.tsx
@@ -42,7 +42,10 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         {data.map((item, index) => (
           <div
             key={index}
-            className="flex justify-start pt-10 md:pt-40 md:gap-10"
+            // eslint-disable-next-line quotes
+            className={`flex justify-start pt-10 ${
+              index !== 0 ? 'md:pt-40' : 'md:pt-8'
+            } md:gap-10`}
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full flex items-center justify-center">
